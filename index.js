04e3fab512b66243a6985aa316ff5aa40fed6d1e
HannoPondaag <hannopondaag@gmail.com>
2025-02-27 22:20:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const result = document.getElementById("result");
 
 function tafelvan(){
    let text = "";
-   for(let i=0;i<10;i++){
+   for(let i = 1; i < 11;i++){
        if(toResult.checked){
-      text += "Dummy" + "<br>";
+      text += i + " x " + textBox.value + " = " + i * textBox.value + "<br>";
   }
    }
    document.getElementById("result").innerHTML = text;
