d8474481457f3c01e815ad2ae019df3b2c940d0a
HannoPondaag <hannopondaag@gmail.com>
2025-02-27 14:46:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+//Tafel van X functionaliteit
+
+const textBox = document.getElementById("textBox");
+const result = document.getElementById("result");
+
+function tafelvan (){
+  
+    result.textContent = "poesje";
+}
 
