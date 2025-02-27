271b764c31d75615f81605ddd68a54edcf523f28
HannoPondaag <hannopondaag@gmail.com>
2025-02-27 20:54:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const textBox = document.getElementById("textBox");
 const toResult = document.getElementById("toResult");
 const result = document.getElementById("result");
 
-let int waarde;
+
 function tafelvan(){
 
-   let int waarde = 2;
+   
   if(toResult.checked){
-    result.textContent = waarde;
+    result.textContent = "Tata! output !";
   }
 }
 
