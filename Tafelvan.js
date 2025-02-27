77988061a9990f0edc2f2717d29d2f14e48d98f4
HannoPondaag <hannopondaag@gmail.com>
2025-02-27 07:39:20 +0100

    Update Tafelvan.js

--- Tafelvan.js
+++ Tafelvan.js
@@ -2,9 +2,9 @@
 
 const textBox=document.getElementById("textBox");
 const result=document.getElementById("result");
-const int value=document.getElementById("value").value;
+var int value=document.getElementById("value").value;
 function tafelvan (){
   for (let i=1;i<11;i++){
-    result.textContent = i + "x" + value + "=" +i*value;
+    result.textContent = "poesje";
 }
 
