ac50157d9521bffa70221e5410e3949970bf5a9d
HannoPondaag <hannopondaag@gmail.com>
2025-02-28 03:15:43 +0100

    Update Sandbox.js

--- Sandbox.js
+++ Sandbox.js
@@ -1,1 +1,17 @@
+//TABLE OF X PROGRAM
+
+const textBox = document.getElementById("textBox");
+const toResult = document.getElementById("toResult");
+const result = document.getElementById("result");
+
+
+function tafelvan(){
+   let text = "";
+   for(let i = 1; i < 11;i++){
+       if(toResult.checked){
+      text += i + " x " + textBox.value + " = " + i * textBox.value + "<br>";
+  }
+   }
+   document.getElementById("result").innerHTML = text;
+}
 
