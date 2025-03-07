9ff5e562a87b2fe84ec97876008eea8d0536d52e
HannoPondaag <hannopondaag@gmail.com>
2025-03-07 17:12:01 +0100

    Update Tafelvan.js

--- Tafelvan.js
+++ Tafelvan.js
@@ -1,10 +1,17 @@
-//Tafel van X functionaliteit
+//TABLE OF X PROGRAM
 
 const textBox = document.getElementById("textBox");
+const toResult = document.getElementById("toResult");
 const result = document.getElementById("result");
 
-function tafelvan (){
-  
-    result.textContent = "poesje";
+
+function tafelvan(){
+   let text = "";
+   for(let i = 1; i < 11;i++){
+       if(toResult.checked){
+      text += i + " x " + textBox.value + " = " + i * textBox.value + "<br>";
+  }
+   }
+   document.getElementById("result").innerHTML = text;
 }
 
